a73365f5d0a206bf6c9aafb5092632fc49e7ced9
aakashcode12 <aakashcoder1210@gmail.com>
2021-05-08 16:11:31 +0530

    TRIGGER DONE

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from accounts.models import Student, Teacher, Course, Lecture, Watch_time,Todo
+from accounts.models import Student, Teacher, Course, Lecture, Watch_time, Todo, Logs
 # Register your models here.
 
 admin.site.register(Student)
@@ -8,3 +8,4 @@ admin.site.register(Course)
 admin.site.register(Lecture)
 admin.site.register(Watch_time)
 admin.site.register(Todo)
+admin.site.register(Logs)
