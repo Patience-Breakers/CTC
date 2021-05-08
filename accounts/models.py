85cb212b6e931d786c9c0c6f7930acd4d7b2576d
aakashcode12 <aakashcoder1210@gmail.com>
2021-05-08 15:58:05 +0530

    Done something

--- accounts/models.py
+++ accounts/models.py
@@ -7,31 +7,34 @@ class Course (models.Model):
     course_name = models.CharField(max_length=100, null=False)
     course_image = models.ImageField(upload_to="course-images/", default="")
     desc = models.CharField(max_length=250, default="")
+
     class Meta:
         ordering = ['course_name']
 
     def _str_(self):
         return self.course_name
 
+
 class Teacher (models.Model):
     teacher_id = models.AutoField(primary_key=True)
     teacher_name = models.CharField(max_length=100, null=False)
-    username = models.CharField(max_length=10, null=False, default ="")
-    password =  models.CharField(max_length=10, null=False,default="")
+    username = models.CharField(max_length=10, null=False, default="")
+    password = models.CharField(max_length=10, null=False, default="")
     teacher_image = models.ImageField(upload_to="teacher-images/", default="")
     teacher_email = models.EmailField(max_length=254, default="")
     teacher_phone = models.CharField(max_length=20, default='')
     teacher_dob = models.DateField(auto_now=False, auto_now_add=False)
-    course =models.ForeignKey(Course, on_delete=models.CASCADE, default="")
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, default="")
 
     def _str_(self):
         return self.teacher_name
 
+
 class Student (models.Model):
     student_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100, null=False)
-    username = models.CharField(max_length=10, null=False, default ="")
-    password =  models.CharField(max_length=10, null=False,default="")
+    username = models.CharField(max_length=10, null=False, default="")
+    password = models.CharField(max_length=10, null=False, default="")
     email = models.EmailField(max_length=254, default="")
     phone_no = models.CharField(max_length=20, default='')
     image = models.ImageField(upload_to="course-images/", default="")
@@ -51,10 +54,10 @@ class Student (models.Model):
 
     def _str_(self):
         return self.name
-    
+
 
 class Lecture (models.Model):
-    lec_id = models.AutoField(primary_key=True) 
+    lec_id = models.AutoField(primary_key=True)
     lec_no = models.CharField(default="0", max_length=100)
     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, default="")
     lecture_name = models.CharField(max_length=100, default="")
@@ -62,64 +65,27 @@ class Lecture (models.Model):
     video = models.FileField(upload_to="videos/", null=True)
     material = models.FileField(upload_to="lec_materials/", null=True)
     thumbnail = models.ImageField(upload_to="course-images/", default="")
+
     def _str_(self):
         return self.lec_no
 
+
 class Watch_time(models.Model):
-  student = models.ForeignKey(Student, on_delete=models.CASCADE, default="")
-  w_lect = models.ForeignKey(Lecture,on_delete=models.CASCADE,default="") 
-  completed = models.BooleanField(default = False)
-  completed_date = models.DateField(auto_now=True, auto_now_add=False)
-  Rating =  models.IntegerField(default=0)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, default="")
+    w_lect = models.ForeignKey(Lecture, on_delete=models.CASCADE, default="")
+    completed = models.BooleanField(default=False)
+    completed_date = models.DateField(auto_now=True, auto_now_add=False)
+    Rating = models.IntegerField(default=0)
 
 
 class Todo(models.Model):
-  student = models.ForeignKey(Student, on_delete=models.CASCADE, default="")
-  task = models.CharField(default="NULL", max_length=100)
-  iscomplete = models.BooleanField(default=False)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, default="")
+    task = models.CharField(default="NULL", max_length=100)
+    iscomplete = models.BooleanField(default=False)
 
 
 # class Todo(models.Model):
-  
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+class Logs(models.Model):
+    student_id = models.IntegerField(primary_key=True)
+    edit_time = models.DateField(auto_now=False, auto_now_add=False)
